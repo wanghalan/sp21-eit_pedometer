6b04ec7c07eeb893db5533df6df1c30619863ce9
Alan <alanwang@usc.edu>
2021-03-14 14:51:57 -0400

    running functionality!

--- pedom/Pedometer/Pedometer.ino
+++ pedom/Pedometer/Pedometer.ino
@@ -2,6 +2,9 @@ int sensorPin = A0;   // voltage input. Use any ADC pin (starts with A)
 int sensorValue = 0;  // stores value from ADC
 int numSteps = 0;
 
+int counter = 0;
+int run_thresh = 5;
+
 void setup()
 {
   Serial.begin(115200);    // Opens serial port, Baud Rate 115200 bps
@@ -16,20 +19,30 @@ void loop()
 
   if (sensorValue > minValue) {
     numSteps++;
-    Serial.print("Steps: ");
-    Serial.println(numSteps);
+//    Serial.print("Steps: ");
+//    Serial.println(numSteps);
     while(sensorValue > minValue) {
-      // Serial.println(sensorValue);
       sensorValue = analogRead (sensorPin);   // reads the sensor
+      counter ++;
+      Serial.print(counter);
+      Serial.print("\t");
       delay(100);
     }
+    if (counter > run_thresh){
+      Serial.print("Walking! - Steps:");
+    }else{
+      Serial.print("Running! - Steps:");
+    }
+    counter = 0;
     numSteps++;
-    Serial.print("Steps: "); 
+//    Serial.print("Steps: "); 
     Serial.println(numSteps);
     //delay(500);
+    delay(100);
+    
   }
 
-  // Serial.println(sensorValue);   // Prints the value via the serial port
+//  Serial.println(sensorValue);   // Prints the value via the serial port
 
   delay(100);
 }
